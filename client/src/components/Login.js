60a7b137df4d02492f528987adfd2e665c211c77
Horacio Garcia <garciahor@gmail.com>
2019-09-24 20:34:57 -0400

    sign  up and log out

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,145 +1,196 @@
 import React from "react";
 import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBInput, MDBBtn, MDBIcon, MDBModalFooter } from 'mdbreact';
 import "./LoginStyle.css"
+import ReactCardFlip from 'react-card-flip';
 
 
 
 class Login extends React.Component {
-    state = {
-        username: "",
-        password: ""
-    }
-
-
-    handleInputChange = event => {
-        const { value, name } = event.target;
-
-        this.setState({
-            [name]: value
-
-        });
-
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: "",
+      isFlipped: false
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
+  handleInputChange = event => {
+    const { value, name } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
 
+  handleClick(e) {
+    e.preventDefault();
+    this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
+  }
 
-    handleFormSubmit = event => {
-        event.preventDefault();
-        if (!this.state.username || !this.state.password) {
-            alert("Enter username and password");
-        } else {
-            localStorage.setItem("username", this.state.username);
-
-        }
-
-        this.setState({
-            username: "",
-            password: ""
-        });
-
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (!this.state.username || !this.state.password) {
+      alert("Enter username and password");
+    } else {
+      localStorage.setItem("username", this.state.username);
     }
+  }
 
-    render() {
-        return (
-            <MDBContainer className="login-container">
-            <MDBRow>
-              <MDBCol md="6">
-                <MDBCard>
-                  <MDBCardBody className="mx-4">
-                    <div className="text-center">
-                      <h3 className="dark-grey-text mb-5">
-                        <strong>Sign in</strong>
-                      </h3>
-                    </div>
-                    <MDBInput
-                     value={this.state.username}
-                     name="username"
-                     onChange={this.handleInputChange}
-                     type="email"
-                      label="Your email"
-                      group
-                      type="email"
-                      validate
-                      error="wrong"
-                      success="right"
-                    />
-                    <MDBInput
-                     value={this.state.password}
-                     name="password"
-                     onChange={this.handleInputChange}
-                     type="password"
-                      label="Your password"
-                      group
-                      type="password"
-                      validate
-                      containerClass="mb-0"
-                    />
-                    <p className="font-small blue-text d-flex justify-content-end pb-3">
-                      Forgot
-                      <a href="#!" className="blue-text ml-1">
+  render() {
+    return (
       
-                        Password?
-                      </a>
-                    </p>
-                    <div className="text-center mb-3">
-                      <MDBBtn
-                        type="button"
-                        gradient="blue"
-                        rounded
-                        className="btn-block z-depth-1a"
-                      >
-                        <button onClick={this.handleFormSubmit}>Sign in</button>
-                      </MDBBtn>
-                    </div>
-                    <p className="font-small dark-grey-text text-right d-flex justify-content-center mb-3 pt-2">
-      
-                      or Sign in with:
-                    </p>
-                    <div className="row my-3 d-flex justify-content-center">
-                      <MDBBtn
-                        type="button"
-                        color="white"
-                        rounded
-                        className="mr-md-3 z-depth-1a"
-                      >
-                        <MDBIcon fab icon="facebook-f" className="blue-text text-center" />
-                      </MDBBtn>
-                      <MDBBtn
-                        type="button"
-                        color="white"
-                        rounded
-                        className="mr-md-3 z-depth-1a"
-                      >
-                        <MDBIcon fab icon="twitter" className="blue-text" />
-                      </MDBBtn>
-                      <MDBBtn
-                        type="button"
-                        color="white"
-                        rounded
-                        className="z-depth-1a"
-                      >
-                        <MDBIcon fab icon="google-plus-g" className="blue-text" />
-                      </MDBBtn>
-                    </div>
-                  </MDBCardBody>
-                  <MDBModalFooter className="mx-5 pt-3 mb-1">
-                    <p className="font-small grey-text d-flex justify-content-end">
-                      Not a member?
-                      <a href="#!" className="blue-text ml-1">
-      
-                        Sign Up
-                      </a>
-                    </p>
-                  </MDBModalFooter>
-                </MDBCard>
-              </MDBCol>
-            </MDBRow>
-          </MDBContainer>
+    
+      <div style={{textAlign: "center"}} >
+        <div style={{display: "inline-block"}}>
 
-        )
+        <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
+          <div key="front">
+            <div>
+              <div className="container">
+                <div className="row">
+                  <div className="col">
+                    <MDBContainer className="login-container">
+                      <MDBRow>
+                        <MDBCol md="6">
+                          <MDBCard style={{width:"500px"}}>
+                            <MDBCardBody className="mx-4">
+                              <div className="text-center">
+                                <h3 className="dark-grey-text mb-5">
+                                  <strong>Sign in</strong>
+                                </h3>
+                              </div>
+                              <MDBInput
+                                value={this.state.username}
+                                name="username"
+                                onChange={this.handleInputChange}
+                                type="email"
+                                label="Your email"
+                                group
+                                type="email"
+                                validate
+                                error="wrong"
+                                success="right"
+                              />
+                              <MDBInput
+                                value={this.state.password}
+                                name="password"
+                                onChange={this.handleInputChange}
+                                type="password"
+                                label="Your password"
+                                group
+                                type="password"
+                                validate
+                                containerClass="mb-0"
+                              />
+                             
+                              <div className="text-center mb-3">
+                                <MDBBtn
+                                  type="button"
+                                  gradient="blue"
+                                  rounded
+                                  className="btn-block z-depth-1a"
+                                >
+                                  <button onClick={this.handleFormSubmit}>Sign in</button>
+                                </MDBBtn>
+                              </div>
+                             
+                             
+                            </MDBCardBody>
+                            <MDBModalFooter className="mx-5 pt-3 mb-1">
+                              <p className="font-small grey-text d-flex justify-content-end">
+                                Not a member?
+                                   <a href="#!" className="blue-text ml-1">
+                                  <MDBBtn onClick={this.handleClick} color="primary" size="md">
+                                    Sign up
+                                  </MDBBtn>
+                                </a>
+                              </p>
+                            </MDBModalFooter>
+                          </MDBCard>
+                        </MDBCol>
+                      </MDBRow>
+                    </MDBContainer>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div key="back">
+            <div className="container">
+              <div className="row">
+                <div className="col">
+                  <MDBContainer>
+                    <MDBRow>
+                      <MDBCol md="6">
+                        <MDBCard  style={{width: "500px"}}>
+                          <MDBCardBody >
+                            <form>
+                              <p className="h4 text-center py-4">Sign up</p>
+                              <div className="grey-text" id="thisidhere">
+                                <MDBInput
+                              
+                                  label="Your name"
+                                  icon="user"
+                                  group
+                                  type="text"
+                                  validate
+                                  error="wrong"
+                                  success="right"
+                                  style={{display: "block"}}
+                                />
+                                <MDBInput
+                               
+                                  label="Your email"
+                                  icon="envelope"
+                                  group
+                                  type="email"
+                                  validate
+                                  error="wrong"
+                                  success="right"
+                                />
+                                <MDBInput
+                               
+                                  label="Confirm your email"
+                                  icon="exclamation-triangle"
+                                  group
+                                  type="text"
+                                  validate
+                                  error="wrong"
+                                  success="right"
+                                />
+                                <MDBInput
+                               
+                                  label="Your password"
+                                  icon="lock"
+                                  group
+                                  type="password"
+                                  validate
+                                />
+                              </div>
+                              <div className="text-center py-4 mt-3">
+                                <MDBBtn onClick={this.handleClick} color="primary" size="md">
+                                  Register
+                                </MDBBtn>
+                              </div>
+                            </form>
+                          </MDBCardBody>
+                        </MDBCard>
+                      </MDBCol>
+                    </MDBRow>
+                  </MDBContainer>
+                  <div className="text-md-right">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </ReactCardFlip>
+      </div>
+      </div>
+    )
 
-    }
+  }
 }
-
 
 export default Login
